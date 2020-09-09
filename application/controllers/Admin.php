f12f7c61b4f3e9eb8a7de8e5aecd9b8dc01a7360
ahmads <ahmadsyaebani1@gmail.com>
2020-09-09 13:42:42 +0700

    Bani-Model dan controler fitur tampil list data kepala desa

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,5 +13,17 @@ class Admin extends CI_Controller {
         $this->load->view('admin/dashboard_admin');
         $this->load->view('footer');
         
-	}
+        }
+
+        public function data_kepaladesa()
+	{
+        $this->load->model('M_admin');
+        $data_kepaladesa = $this->M_admin->get_data_kepaladesa();
+        $result = array(
+                'list_data' => $data_kepaladesa,
+                'page' => 'admin/list_kepaladesa',
+        );
+        $this->load->view('header','admin/sidebar_admin','topbar',$result,'footer');
+        }
+        
 }

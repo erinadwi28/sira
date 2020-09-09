3fff2fbb4dbeb49ecfb27b1a9f1997543689446f
ahmads <ahmadsyaebani1@gmail.com>
2020-09-09 19:06:09 +0700

    Bani- perbarui M_admin ke php

--- application/models/M_admin.php
+++ application/models/M_admin.php
@@ -8,4 +8,15 @@ class M_admin extends CI_Model{
 		$query = $this->db->get_where($this->_table, ['nik' => $nik]);
 		return $query->row();
 	}
+	
+	// list data kepala desa
+	 public function get_data_kepaladesa(){
+        $this->db->select('*');
+        $this->db->from('kepala_desa');
+        $this->db->where('status_delete', 0);
+
+        $hasil = $this->db->get();
+
+        return $hasil;
+    }
 }

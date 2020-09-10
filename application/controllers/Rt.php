a6d4ae3b277d6c91aa09120c3d9e1330199608e0
rahmau <rahmamaulani26@gmail.com>
2020-09-10 16:26:02 +0700

    rahma

--- application/controllers/Rt.php
+++ application/controllers/Rt.php
@@ -1,18 +1,300 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Rt extends CI_Controller {
+class Rt extends CI_Controller
+{
 
-	
+
 	public function index()
-	{   
-
-        $this->load->view('header');
-        $this->load->view('rt/sidebar_rt');
-        $this->load->view('topbar');
-        $this->load->view('rt/dashboard_rt');
-        $this->load->view('footer');
-        
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/dashboard_rt');
+		$this->load->view('footer');
 	}
+	public function profil_rt()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/profil_rt');
+		$this->load->view('footer');
+	}
+	public function ubah_profil_rt()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/ubah_profil_rt');
+		$this->load->view('footer');
+	}
+	public function list_data_permohonan_rt()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/list_data_permohonan_rt');
+		$this->load->view('footer');
+	}
+	public function list_data_disetujui_rt()
+	{
 
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/list_data_disetujui_rt');
+		$this->load->view('footer');
+	}
+	public function history_permohonan_surat_rt()
+	{
+
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/history_permohonan_surat_rt');
+		$this->load->view('footer');
+	}
+	public function list_data_warga_rt()
+	{
+
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/list_data_warga_rt');
+		$this->load->view('footer');
+	}
+	public function detail_data_warga()
+	{
+
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/detail_data_warga');
+		$this->load->view('footer');
+	}
+	public function form_suket001()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_001/form_suket001');
+		$this->load->view('footer');
+	}
+	public function detail_suket001()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_001/detail_suket001');
+		$this->load->view('footer');
+	}
+	public function form_suket002()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_002/form_suket002');
+		$this->load->view('footer');
+	}
+	public function detail_suket002()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_002/detail_suket002');
+		$this->load->view('footer');
+	}
+	public function form_suket003()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_003/form_suket003');
+		$this->load->view('footer');
+	}
+	public function detail_suket003()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_003/detail_suket003');
+		$this->load->view('footer');
+	}
+	public function form_suket004()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_004/form_suket004');
+		$this->load->view('footer');
+	}
+	public function detail_suket004()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_004/detail_suket004');
+		$this->load->view('footer');
+	}
+	public function form_suket005()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_005/form_suket005');
+		$this->load->view('footer');
+	}
+	public function detail_suket005()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_005/detail_suket005');
+		$this->load->view('footer');
+	}
+	public function form_suket006()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_006/form_suket006');
+		$this->load->view('footer');
+	}
+	public function detail_suket006()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_006/detail_suket006');
+		$this->load->view('footer');
+	}
+	public function form_suket007()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_007/form_suket007');
+		$this->load->view('footer');
+	}
+	public function detail_suket007()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_007/detail_suket007');
+		$this->load->view('footer');
+	}
+	public function form_suket008()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_008/form_suket008');
+		$this->load->view('footer');
+	}
+	public function detail_suket008()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_008/detail_suket008');
+		$this->load->view('footer');
+	}
+	public function form_suket009()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_009/form_suket009');
+		$this->load->view('footer');
+	}
+	public function detail_suket009()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_009/detail_suket009');
+		$this->load->view('footer');
+	}
+	public function form_suket010()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_010/form_suket010');
+		$this->load->view('footer');
+	}
+	public function detail_suket010()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_001/detail_suket010');
+		$this->load->view('footer');
+	}
+	public function form_suket011()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_011/form_suket011');
+		$this->load->view('footer');
+	}
+	public function detail_suket011()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_011/detail_suket011');
+		$this->load->view('footer');
+	}
+	public function form_suket012()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_012/form_suket012');
+		$this->load->view('footer');
+	}
+	public function detail_suket012()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_012/detail_suket012');
+		$this->load->view('footer');
+	}
+	public function form_suket013()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_013/form_suket013');
+		$this->load->view('footer');
+	}
+	public function detail_suket013()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_013/detail_suket013');
+		$this->load->view('footer');
+	}
+	public function form_suket014()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_014/form_suket014');
+		$this->load->view('footer');
+	}
+	public function detail_suket014()
+	{
+		$this->load->view('header');
+		$this->load->view('rt/sidebar_rt');
+		$this->load->view('topbar');
+		$this->load->view('rt/suket_014/detail_suket014');
+		$this->load->view('footer');
+	}
 }
---
+++ application/views/rt/detail_data_warga.php
@@ -0,0 +1,166 @@
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                	<!-- Page Heading -->
+                	<div class="d-sm-flex align-items-center justify-content-between mb-4">
+                		<h1 class="h3 mb-0 text-gray-800">Detail Data</h1>
+                	</div>
+
+                	<!-- Content Row line 1-->
+                	<div class="row">
+                		<div class="col-md-4 mb-4">
+                			<!-- Foto -->
+                			<div class="card shadow mb-4">
+                				<div class="card-body">
+                					<img src="img/wkwk.jpg" alt="foto profil" class="img-fluid">
+                				</div>
+                				<div class="card-footer py-3">
+                					<center>
+                						<h6 class="m-0 font-weight-bold text-primary">Erina Dwi Utami</h6>
+                					</center>
+                				</div>
+                			</div>
+                		</div>
+                		<div class="col-md-8 mb-4">
+                			<!-- Detail Data -->
+                			<div class="card shadow mb-4">
+                				<div class="card-body">
+                					<table class="table-hover table-responsive">
+                						<tbody>
+                							<tr>
+                								<td><b>NIK</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>123456789</td>
+                							</tr>
+                							<tr>
+                								<td><b>Nama Lengkap</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Erina Dwi Utami</td>
+                							</tr>
+                							<tr>
+                								<td><b>Tempat Lahir</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Sleman</td>
+                							</tr>
+                							<tr>
+                								<td><b>Tanggal Lahir</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>28/02/2000</td>
+                							</tr>
+                							<tr>
+                								<td><b>Jenis Kelamin</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Perempuan</td>
+                							</tr>
+                							<tr>
+                								<td><b>Golongan Darah</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>AB</td>
+                							</tr>
+                							<tr>
+                								<td><b>Alamat</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Mudal</td>
+                							</tr>
+                							<tr>
+                								<td><b>RT</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>005</td>
+                							</tr>
+                							<tr>
+                								<td><b>Kelurahan</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Argomulyo</td>
+                							</tr>
+                							<tr>
+                								<td><b>Kecamatan</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Cangkringan</td>
+                							</tr>
+                							<tr>
+                								<td><b>Agama</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Islam</td>
+                							</tr>
+                							<tr>
+                								<td><b>Status Perkawinan</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Belum Kawin</td>
+                							</tr>
+                							<tr>
+                								<td><b>Pekerjaan</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Pelajar/Mahasiswa</td>
+                							</tr>
+                							<tr>
+                								<td><b>Kewarganegaraan</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>WNI</td>
+                							</tr>
+                							<tr>
+                								<td><b>Nomor Kartu Keluarga</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>340429377111</td>
+                							</tr>
+                							<tr>
+                								<td><b>Pendidikan Terakhir</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>SMK</td>
+                							</tr>
+                							<tr>
+                								<td><b>Status Hubungan Dalam Keluarga</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>Anak</td>
+                							</tr>
+                							<tr>
+                								<td><b>Nomor HandPhone</b></td>
+                								<td> </td>
+                								<td> </td>
+                								<td> </td>
+                								<td>085713609299</td>
+                							</tr>
+                						</tbody>
+                					</table>
+                				</div>
+                			</div>
+                		</div>
+
+
+                	</div>
+                </div>
+                <!-- /.container-fluid -->
+                </div>
+                <!-- End of Main Content -->
